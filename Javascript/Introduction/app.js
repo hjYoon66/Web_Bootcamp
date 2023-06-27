4350e408337f0f4d2bda6030f9cd41dae6ca0304
Yoon_98 <poiu601@naver.com>
2023-06-27 22:02:50 +0900

    Javascript Basic Ing

--- Javascript/Introduction/app.js
+++ Javascript/Introduction/app.js
@@ -1,5 +1,15 @@
-let greetingText="Hi I'm Yoon!";
+let userName="Yoon";
 let age=26;
 let hobbies=['Sports','Cooking','Reading'];
+let job={
+    title:'Developer',
+    place:'Seoul',
+    salary:50000
+};
 
-alert(hobbies[0]);
+let adultYears = age-18;
+alert (adultYears);
+
+age=45;
+adultYears=age-18;
+alert (adultYears);

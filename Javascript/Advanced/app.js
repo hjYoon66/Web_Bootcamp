704fa6204a0cc13db2bfa1d623fb90f53631bfa4
Yoon_98 <poiu601@naver.com>
2023-06-29 01:26:58 +0900

    Javascript Advanced Finished

---
+++ Javascript/Advanced/app.js
@@ -0,0 +1,7 @@
+//document.body.children[1].children[0].href='https://google.com';
+
+let anchorElement=document.getElementById('external-link');
+anchorElement.href='https://google.com';
+
+anchorElement=document.querySelector('#external-id');
+anchorElement.href='https://naver.com';

a148508ecabe8481f1b5a919a88127c83c8f8b36
Yoon_98 <101798354+Yoon0601@users.noreply.github.com>
2023-07-12 22:14:15 +0900

    Tic,Tac,Toe Game Start

---
+++ Javascript/Tic-Tac-Toe-game/scripts/config.js
@@ -0,0 +1,9 @@
+function openPlayerConfig() {
+    playerConfigOVerlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOVerlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+}

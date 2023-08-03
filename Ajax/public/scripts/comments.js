313d7b0bfb06a47e17b9116930792c19ebbf756f
Yoon_98 <101798354+Yoon0601@users.noreply.github.com>
2023-08-03 15:08:30 +0900

    Ajax Finished

--- Ajax/public/scripts/comments.js
+++ Ajax/public/scripts/comments.js
@@ -1,10 +1,80 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
+const commentsSectionElement = document.getElementById('comments');
+const commentsFormElement = document.querySelector('#comments-form form');
+const commentTitleElement = document.getElementById('title');
+const commentTextElement = document.getElementById('text');
 
-async function fetchCommentsForPost(){
-    const postId = loadCommentsBtnElement.dataset.postId;
-    const response = await fetch(`/posts/${postId}/comments`);
-    const responseData = await response.json();
-    console.log(responseData);
+
+function createCommentsList(comments) {
+    const commentsListElement = document.createElement('ol');
+
+    for (const comment of comments) {
+        const commentElement = document.createElement('li');
+        commentElement.innerHTML = `
+        <article class="comment-item">
+            <h2>${comment.title}</h2>
+            <p>${comment.text}</p>
+        </article>
+        `;
+        commentsListElement.appendChild(commentElement);
+    }
+    return commentsListElement;
+}
+async function fetchCommentsForPost() {
+    const postId = loadCommentsBtnElement.dataset.postid;
+
+    try {
+        const response = await fetch(`/posts/${postId}/comments`);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            alert('Fetching comments failed')
+            return;
+        }
+
+        if (responseData && responseData.length > 0) {
+            const commentsListElement = createCommentsList(responseData);
+            commentsSectionElement.innerHTML = '';
+            commentsSectionElement.appendChild(commentsListElement);
+        } else {
+            commentsSectionElement.firstElementChild.textContent =
+                'We could not find any comments. Maybe add one?';
+        }
+    } catch (error) {
+        alert('Getting comments failed');
+    }
+
 }
 
-loadCommentsBtnElement.addEventListener('click',fetchCommentsForPost);
+async function saveComment(event) {
+    event.preventDefault();
+    const postId = commentsFormElement.dataset.postid;
+
+    const enteredTitle = commentTitleElement.value;
+    const enteredText = commentTextElement.value;
+
+    const comment = { title: enteredTitle, text: enteredText };
+
+    try {
+        const response = await fetch(`/posts/${postId}/comments`, {
+            method: 'POST',
+            body: JSON.stringify(comment),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            fetchCommentsForPost();
+        } else {
+            alert('Could not send comment!');
+        }
+    } catch (error) {
+        alert('Could not send request - maybe try again later')
+
+    }
+
+}
+
+loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
+commentsFormElement.addEventListener('submit', saveComment);

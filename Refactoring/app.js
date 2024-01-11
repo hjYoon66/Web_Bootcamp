24372976ca773faefafdb50754bcdbe52f26dd7b
hjyoon66 <poiu601@naver.com>
2024-01-12 02:33:35 +0900

    Refactoring apply  :mega:

--- Refactoring/app.js
+++ Refactoring/app.js
@@ -1,24 +1,25 @@
-const path = require('path');
+// 리팩터링 실시
+const path = require("path");
 
-const express = require('express');
-const session = require('express-session');
-const csrf = require('csurf');
+const express = require("express");
+const session = require("express-session");
+const csrf = require("csurf");
 
-const sessionConfig = require('./config/session')
-const db = require('./data/database');
-const authRoutes = require('./routes/auth')
-const blogRoutes = require('./routes/blog');
-const authMiddleware = require('./middlewares/auth-middleware');
-const addCSRFTokenMiddleware = require('./middlewares/csrf-token-middleware')
+const sessionConfig = require("./config/session");
+const db = require("./data/database");
+const authRoutes = require("./routes/auth");
+const blogRoutes = require("./routes/blog");
+const authMiddleware = require("./middlewares/auth-middleware");
+const addCSRFTokenMiddleware = require("./middlewares/csrf-token-middleware");
 
 const mongoDbSessionStore = sessionConfig.createSessionStore(session);
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(session(sessionConfig.createSessionConfig(mongoDbSessionStore)));
@@ -30,9 +31,9 @@ app.use(authMiddleware);
 app.use(authRoutes);
 app.use(blogRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);

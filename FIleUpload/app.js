9f88f7b1e6d067bec01fb9de9354a1fef425a85b
hjyoon66 <poiu601@naver.com>
2024-01-12 02:27:56 +0900

    FileUpload Practice  :file_folder:

--- FIleUpload/app.js
+++ FIleUpload/app.js
@@ -1,18 +1,19 @@
-const path = require('path');
+// 파일 업로드 해보기
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const userRoutes = require('./routes/users');
-const db = require('./data/database');
+const userRoutes = require("./routes/users");
+const db = require("./data/database");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use('/images', express.static('images'));
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
 
 app.use(userRoutes);
 

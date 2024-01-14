795a6d745708de0268ea45a73f404bd852d45fcf
hjyoon66 <poiu601@naver.com>
2024-01-14 21:46:22 +0900

    Validation  work :running:

--- Complete-Shoppingmall/models/user.model.js
+++ Complete-Shoppingmall/models/user.model.js
@@ -18,6 +18,14 @@ class User {
     return db.getDb().collection("users").findOne({ email: this.email });
   }
 
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
+    }
+    return false;
+  }
+
   async signup() {
     const hashedPassword = await bcypt.hash(this.password, 12);
 

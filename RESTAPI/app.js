e66fb5eb3caf3b908b81de15d09d2c766cd4de28
hjyoon66 <poiu601@naver.com>
2024-01-22 21:37:23 +0900

    REST API work :heart:

---
+++ RESTAPI/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const db = require("./data/database");
+
+const quoteRoutes = require("./routes/quotes.routes");
+
+const app = express();
+
+app.use("/quote", quoteRoutes);
+
+app.use(function (error, req, res, next) {
+  res.status(500).json({
+    message: "something went wrong!",
+  });
+});
+
+db.initDb()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Connectiong to the database failed!");
+  });
